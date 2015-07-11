1f0cc7309f5fafc28d32f5c8c0f898f1414c2286
Jeffrey Blagdon <jeffblagdon@gmail.com>
2015-07-11 10:20:48 -0400

    Initial commit

---
+++ wine-pairings/LandingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wine-pairings
+//
+//  Created by Jeffrey Blagdon on 7/11/15.
+//  Copyright (c) 2015 Jeffrey Blagdon. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
